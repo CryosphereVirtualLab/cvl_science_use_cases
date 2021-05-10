0df081acc1a164c046e791ab1822fb1aedd7ed07
azamifarda <arash.azamifard@nersc.no>
2021-05-10 17:25:50 +0200

    #11 minors

--- UseCase2/containers/run_docker.sh
+++ UseCase2/containers/run_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm -d -v /Users/arazam/nansen_code/CVL/use_cases/cvl_science_use_cases/UseCase2/notebooks:/opt/notebooks -p 8888:8888 use_case_2_jupyter
+docker run --rm -d -v /srv/home/<Ptep_portal_username>/proc_tutorial_shell/notebooks:/opt/notebooks -p 8888:8888  use_case_2_jupyter
